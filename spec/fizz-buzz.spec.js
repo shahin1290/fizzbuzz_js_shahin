0ff84a4f41d34ae5304304a109eff0d602f52e90
shahin1290 <s.patowary@yahoo.com>
2018-11-11 13:42:31 +0100

    Creates spec and adds function to js file so that it returns fizzbuzz if number is divisible by 15

--- spec/fizz-buzz.spec.js
+++ spec/fizz-buzz.spec.js
@@ -14,4 +14,8 @@ describe('Fizz Buzz', () => {
     it('returns Buzz if number is divisible by 5', () => {
         expect(fizzBuzz.check(5)).to.eql('Buzz')
     })
+
+    it('returns FizzBuzz if number is divisible by 15', () => {
+        expect(fizzBuzz.check(15)).to.eql('FizzBuzz')
+    })
 })
